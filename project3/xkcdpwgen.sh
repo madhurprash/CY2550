120c2a2707ec9016762d52f32985e3548ee2815c
madhurprash <129979633+madhurprash@users.noreply.github.com>
2023-06-12 20:23:09 -0400

    Delete xkcdpwgen.sh

--- project3/xkcdpwgen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Represents compiling the java source file to get the java program to run
-javac xkcdpwgen.java
-
-## Represents running the program given below:
-java xkcdpwgen "$@"
